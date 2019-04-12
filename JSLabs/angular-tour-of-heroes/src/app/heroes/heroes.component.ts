2b445027c7f8c5d57d9a22f844dad04644c23f13
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-04-12 15:11:11 -0400

    Working on http section

--- JSLabs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ JSLabs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
 import {HeroService} from '../hero.service';
+import {subscribeOn} from 'rxjs/operators';
 
 
 @Component({
@@ -11,17 +12,24 @@ import {HeroService} from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+
+
+  constructor(private heroService: HeroService) {
   }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
-
-constructor(private heroService: HeroService) {}
-
-ngOnInit() {}
-
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
 }
